205e15f1f103ba4c7adb723900fa8b5ba0c9c791
Bumpsh <54717570+Bumpsh@users.noreply.github.com>
2020-05-19 21:39:09 -0700

    Add files via upload

---
+++ AID_python.py
@@ -0,0 +1,4 @@
+
+print ('this is a AID (active interface design)')
+print ('this A.I.D is all open source and you can bend it however you want')
+print ('feel free to customize every module and even add new ones!')
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Hello please run these commands:
+
+sudo chmod +x init.AID
+
+sudo ./init.AID
+ 
+ 
